31486f524393aaa3a004f8e170eb681683fa9661
Luca Ferrari <fluca1978@gmail.com>
2018-10-31 09:38:31 +0100

    Add more randomness to avoid duplicate key failure

--- chapter_02/Chapter2_Listing07.sql
+++ chapter_02/Chapter2_Listing07.sql
@@ -1,6 +1,6 @@
 INSERT INTO files( f_name, f_hash, f_size )
-SELECT 'file_' || v || '.txt',
-       md5( v::text ),
-       v * ( random() * 100 )::int
-FROM generate_series(1, 10 ) v
+SELECT 'File_' || v || '.txt',
+       md5( v::text || random() ),
+       v * ( random() * 111 )::int
+FROM generate_series( 1, 10 ) v
 RETURNING pk, f_name, f_hash, f_size, ts;
